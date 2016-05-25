574783c07f99576e2c293771c87f8c97dca78305
N1ne999 <get9719@gmail.com>
2016-05-26 00:45:33 +0600

    add root_h

---
+++ src/root.h
@@ -0,0 +1,17 @@
+#ifndef ROOT_H
+#define ROOT_H
+
+#define TWO_ROOTS 2
+#define ONE_ROOT 1
+#define NO_ROOTS 0
+#define INVALID_INPUT -1
+
+typedef struct {
+	float x1;
+	float x2;
+	float discriminant;
+} Roots;
+
+Roots proc(float a, float b, float c, int *stat);
+
+#endif
